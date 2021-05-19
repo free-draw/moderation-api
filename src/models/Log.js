2bc7f64ed8c08b0863567f4dc4478add57d6c45b
Reselim <notreselim@gmail.com>
2021-05-19 05:40:20 -0400

    Index log time

--- src/models/Log.js
+++ src/models/Log.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 const LogType = require("../enum/LogType")
 
 const LogSchema = new mongoose.Schema({
-	timestamp: { type: Date, default: Date.now },
+	timestamp: { type: Date, default: Date.now, index: true },
 	type: { type: String, enum: Object.keys(LogType), index: true },
 	identity: { type: mongoose.Types.ObjectId, ref: "Moderator" },
 	data: Object,
