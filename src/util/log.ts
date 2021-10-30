4a2b64872e810e324857f276145a8bc65dc6577b
Reselim <notreselim@gmail.com>
2021-10-30 01:37:55 -0400

    Remove prettyPrint; replace with pino-pretty command

--- src/util/log.ts
+++ src/util/log.ts
@@ -1,8 +1,6 @@
 import pino from "pino"
 import args from "./option/args"
-import env from "./option/env"
 
 export default pino({
 	level: args.verbose > 0 ? "debug" : "info",
-	prettyPrint: env.nodeEnv !== "production",
 })
