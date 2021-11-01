04166370e87c6bd2518ee2b46dcf7d19a1d73f2d
Reselim <notreselim@gmail.com>
2021-11-01 05:22:00 -0400

    Add pagination for getLogs

--- packages/client/src/method/logs/getLogs.ts
+++ packages/client/src/method/logs/getLogs.ts
@@ -3,6 +3,26 @@ import SortDirection from "../../enum/SortDirection"
 import API from "../../API"
 import Log, { LogData } from "../../class/Log"
 
+class LogsPage {
+	private nextPageCursor: string
+	private previousPageCursor: string
+	public logs: Log[]
+
+	constructor(nextPageCursor: string, previousPageCursor: string, logs: LogData[]) {
+		this.nextPageCursor = nextPageCursor
+		this.previousPageCursor = previousPageCursor
+		this.logs = logs.map(logData => new Log(logData))
+	}
+
+	public async next(api: API): Promise<LogsPage> {
+		return await getLogs(api, { cursor: this.nextPageCursor })
+	}
+
+	public async previous(api: API): Promise<LogsPage> {
+		return await getLogs(api, { cursor: this.previousPageCursor })
+	}
+}
+
 type GetLogsResponse = {
 	nextPageCursor: string,
 	previousPageCursor: string,
@@ -18,14 +38,14 @@ async function getLogs(api: API, options?: {
 	after?: string,
 	before?: string,
 	size?: string,
-}): Promise<Log[]> {
+}): Promise<LogsPage> {
 	const { data } = await api.request<GetLogsResponse>({
 		url: "/logs",
 		method: "GET",
 		params: options,
 	})
 
-	return data.logs.map(logData => new Log(logData))
+	return new LogsPage(data.nextPageCursor, data.previousPageCursor, data.logs)
 }
 
 export default getLogs
