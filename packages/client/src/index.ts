12daadbf7714ed4baf983e2fac3d22fa1bf76cae
Reselim <notreselim@gmail.com>
2021-11-09 05:45:38 -0500

    client: Export Parsed types

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -4,6 +4,8 @@ export * from "./primitive"
 export * from "./enum"
 export * from "./type"
 
+export { ParsedLine, ParsedLayer, ParsedFile } from "./saveFormat"
+
 export { default as API } from "./API"
 export { default as Resource } from "./Resource"
 export { default as Realtime } from "./Realtime"
