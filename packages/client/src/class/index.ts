aa51ad332af5373284a4b32ce36d6291ac888632
Reselim <notreselim@gmail.com>
2021-11-11 20:26:14 -0500

    client: Export LogTypeData

--- packages/client/src/class/index.ts
+++ packages/client/src/class/index.ts
@@ -1,5 +1,5 @@
 export { default as Action, ActionData, ActionOptions } from "./Action"
-export { default as Log, LogData } from "./Log"
+export { default as Log, LogData, LogTypeData } from "./Log"
 export { default as Moderator, ModeratorData } from "./Moderator"
 export { default as ModeratorAccount, ModeratorAccountData } from "./ModeratorAccount"
 export { default as Report, ReportData, ReportOptions } from "./Report"
