3f34b9080d9eb99622b34fc5f3bafadfd7a8dc94
Reselim <notreselim@gmail.com>
2021-11-19 20:53:03 -0500

    client: Export Page and Resolvable from types

--- packages/client/src/type/index.ts
+++ packages/client/src/type/index.ts
@@ -1,2 +1,5 @@
 export { default as RobloxUser } from "./RobloxUser"
 export { default as Token } from "./Token"
+
+export { default as Page } from "./interface/Page"
+export { default as Resolvable } from "./interface/Resolvable"
